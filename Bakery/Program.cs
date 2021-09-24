4a6debc7f628d6348e06356d6c4c0764acc3e6ce
Nathan Fletcher <nathan.fletcher@gmail.com>
2021-09-24 13:53:49 -0700

    Add UI functions to add to order

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -10,38 +10,53 @@ public class Program
     Console.WriteLine($"Today's deals:");
     Console.WriteLine($"Bread: $5 a loaf. Buy 2 get 1 free!");
     Console.WriteLine($"Pastries: $2 each. Buy 2 get 1 half off!");
+    
+    InteractWithCustomer();
 
-    SellBread();
-    SellPastries();
-    DisplayOrder();
+    void InteractWithCustomer()
+    {
+      SellBread();
+      SellPastries();
+      DisplayOrder();
+      AskWhetherToModify();
+    }
 
-    // void AddToOrder()
     void SellBread()
     {
-      Console.WriteLine($"How many loaves would you like to buy?");
+      Console.WriteLine($"How many loaves would you like to add to your order?");
       int loavesInput = int.Parse(Console.ReadLine());
       Bread.Add(loavesInput);
     }
 
     void SellPastries()
     {
-      Console.WriteLine($"How many pastries would you like to buy?");
+      Console.WriteLine($"How many pastries would you like to add to your order?");
       int pastriesInput = int.Parse(Console.ReadLine());
       Pastry.Add(pastriesInput);
     }  
 
     void DisplayOrder()
     {
-      //Display items: 
       Console.WriteLine($"Your order: ");
       foreach (BakeryItem item in BakeryItem.OrderItems)
       {
-        Console.WriteLine($"{item.GetType()}");
+        Console.WriteLine($"{item.Name}");
       }
       Console.WriteLine($"Total: ${BakeryItem.GetOrderCost()}.");
     }
 
-
-    
+    void AskWhetherToModify()
+    {
+      Console.WriteLine($"Would you like to add to your order? (Y/N)");
+      string input = Console.ReadLine();
+      if(input.ToLower() == "y")
+      {
+        InteractWithCustomer();
+      }
+      else
+      {
+        Console.WriteLine($"Thanks for visiting Nathan's! Come back anytime!");
+      }
+    }
   }
 }
